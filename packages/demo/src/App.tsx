b384af5b213fffc7f0f5fd5b39b1174470742ea1
Arpit Tomar <arpit.tomar@2mee.com>
2022-02-07 15:00:19 +0530

    feat: downgrade the node version

--- packages/demo/src/App.tsx
+++ packages/demo/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import { Button, Heading, Text, Text2 } from '@asf-ui/common';
-import { Heading2 } from '@asf-ui/main';
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Text></Text>
       <Heading></Heading>
       <Text2></Text2>
-      <Heading2></Heading2>
     </div>
   );
 }
